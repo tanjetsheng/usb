cc9c33d4d94af690b02d84b91738f53b2f6d3a2a
tanjetsheng <tanjetsheng@gmail.com>
2018-06-20 14:57:22 +0800

    usb receive and send sample data

---
+++ Debug/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_ll_usb.d
@@ -0,0 +1,74 @@
+Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_ll_usb.o: \
+ ../Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_ll_usb.c \
+ C:/git/usb_Spi/Usb/usb/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h \
+ C:/git/usb_Spi/Usb/usb/Inc/stm32f1xx_hal_conf.h \
+ C:/git/usb_Spi/Usb/usb/Inc/main.h \
+ C:/git/usb_Spi/Usb/usb/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h \
+ C:/git/usb_Spi/Usb/usb/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_def.h \
+ C:/git/usb_Spi/Usb/usb/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h \
+ C:/git/usb_Spi/Usb/usb/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h \
+ C:/git/usb_Spi/Usb/usb/Drivers/CMSIS/Include/core_cm3.h \
+ C:/git/usb_Spi/Usb/usb/Drivers/CMSIS/Include/core_cmInstr.h \
+ C:/git/usb_Spi/Usb/usb/Drivers/CMSIS/Include/cmsis_gcc.h \
+ C:/git/usb_Spi/Usb/usb/Drivers/CMSIS/Include/core_cmFunc.h \
+ C:/git/usb_Spi/Usb/usb/Drivers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h \
+ C:/git/usb_Spi/Usb/usb/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h \
+ C:/git/usb_Spi/Usb/usb/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio.h \
+ C:/git/usb_Spi/Usb/usb/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio_ex.h \
+ C:/git/usb_Spi/Usb/usb/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma.h \
+ C:/git/usb_Spi/Usb/usb/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma_ex.h \
+ C:/git/usb_Spi/Usb/usb/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_cortex.h \
+ C:/git/usb_Spi/Usb/usb/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h \
+ C:/git/usb_Spi/Usb/usb/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h \
+ C:/git/usb_Spi/Usb/usb/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h \
+ C:/git/usb_Spi/Usb/usb/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pcd.h \
+ C:/git/usb_Spi/Usb/usb/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_ll_usb.h \
+ C:/git/usb_Spi/Usb/usb/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pcd_ex.h
+
+C:/git/usb_Spi/Usb/usb/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h:
+
+C:/git/usb_Spi/Usb/usb/Inc/stm32f1xx_hal_conf.h:
+
+C:/git/usb_Spi/Usb/usb/Inc/main.h:
+
+C:/git/usb_Spi/Usb/usb/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h:
+
+C:/git/usb_Spi/Usb/usb/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_def.h:
+
+C:/git/usb_Spi/Usb/usb/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h:
+
+C:/git/usb_Spi/Usb/usb/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h:
+
+C:/git/usb_Spi/Usb/usb/Drivers/CMSIS/Include/core_cm3.h:
+
+C:/git/usb_Spi/Usb/usb/Drivers/CMSIS/Include/core_cmInstr.h:
+
+C:/git/usb_Spi/Usb/usb/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+C:/git/usb_Spi/Usb/usb/Drivers/CMSIS/Include/core_cmFunc.h:
+
+C:/git/usb_Spi/Usb/usb/Drivers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h:
+
+C:/git/usb_Spi/Usb/usb/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h:
+
+C:/git/usb_Spi/Usb/usb/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio.h:
+
+C:/git/usb_Spi/Usb/usb/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio_ex.h:
+
+C:/git/usb_Spi/Usb/usb/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma.h:
+
+C:/git/usb_Spi/Usb/usb/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma_ex.h:
+
+C:/git/usb_Spi/Usb/usb/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_cortex.h:
+
+C:/git/usb_Spi/Usb/usb/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h:
+
+C:/git/usb_Spi/Usb/usb/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h:
+
+C:/git/usb_Spi/Usb/usb/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h:
+
+C:/git/usb_Spi/Usb/usb/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pcd.h:
+
+C:/git/usb_Spi/Usb/usb/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_ll_usb.h:
+
+C:/git/usb_Spi/Usb/usb/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pcd_ex.h:
---
+++ Debug/Middlewares/ST/STM32_USB_Device_Library/Core/Src/usbd_ioreq.d
@@ -0,0 +1,95 @@
+Middlewares/ST/STM32_USB_Device_Library/Core/Src/usbd_ioreq.o: \
+ ../Middlewares/ST/STM32_USB_Device_Library/Core/Src/usbd_ioreq.c \
+ C:/git/usb_Spi/Usb/usb/Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_ioreq.h \
+ C:/git/usb_Spi/Usb/usb/Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_def.h \
+ C:/git/usb_Spi/Usb/usb/Inc/usbd_conf.h \
+ C:/git/usb_Spi/Usb/usb/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h \
+ C:/git/usb_Spi/Usb/usb/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h \
+ C:/git/usb_Spi/Usb/usb/Drivers/CMSIS/Include/core_cm3.h \
+ C:/git/usb_Spi/Usb/usb/Drivers/CMSIS/Include/core_cmInstr.h \
+ C:/git/usb_Spi/Usb/usb/Drivers/CMSIS/Include/cmsis_gcc.h \
+ C:/git/usb_Spi/Usb/usb/Drivers/CMSIS/Include/core_cmFunc.h \
+ C:/git/usb_Spi/Usb/usb/Drivers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h \
+ C:/git/usb_Spi/Usb/usb/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h \
+ C:/git/usb_Spi/Usb/usb/Inc/stm32f1xx_hal_conf.h \
+ C:/git/usb_Spi/Usb/usb/Inc/main.h \
+ C:/git/usb_Spi/Usb/usb/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h \
+ C:/git/usb_Spi/Usb/usb/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_def.h \
+ C:/git/usb_Spi/Usb/usb/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h \
+ C:/git/usb_Spi/Usb/usb/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio.h \
+ C:/git/usb_Spi/Usb/usb/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio_ex.h \
+ C:/git/usb_Spi/Usb/usb/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma.h \
+ C:/git/usb_Spi/Usb/usb/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma_ex.h \
+ C:/git/usb_Spi/Usb/usb/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_cortex.h \
+ C:/git/usb_Spi/Usb/usb/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h \
+ C:/git/usb_Spi/Usb/usb/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h \
+ C:/git/usb_Spi/Usb/usb/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h \
+ C:/git/usb_Spi/Usb/usb/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pcd.h \
+ C:/git/usb_Spi/Usb/usb/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_ll_usb.h \
+ C:/git/usb_Spi/Usb/usb/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pcd_ex.h \
+ C:/git/usb_Spi/Usb/usb/Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_def.h \
+ C:/git/usb_Spi/Usb/usb/Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_core.h \
+ C:/git/usb_Spi/Usb/usb/Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_ioreq.h \
+ C:/git/usb_Spi/Usb/usb/Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_ctlreq.h
+
+C:/git/usb_Spi/Usb/usb/Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_ioreq.h:
+
+C:/git/usb_Spi/Usb/usb/Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_def.h:
+
+C:/git/usb_Spi/Usb/usb/Inc/usbd_conf.h:
+
+C:/git/usb_Spi/Usb/usb/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h:
+
+C:/git/usb_Spi/Usb/usb/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h:
+
+C:/git/usb_Spi/Usb/usb/Drivers/CMSIS/Include/core_cm3.h:
+
+C:/git/usb_Spi/Usb/usb/Drivers/CMSIS/Include/core_cmInstr.h:
+
+C:/git/usb_Spi/Usb/usb/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+C:/git/usb_Spi/Usb/usb/Drivers/CMSIS/Include/core_cmFunc.h:
+
+C:/git/usb_Spi/Usb/usb/Drivers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h:
+
+C:/git/usb_Spi/Usb/usb/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h:
+
+C:/git/usb_Spi/Usb/usb/Inc/stm32f1xx_hal_conf.h:
+
+C:/git/usb_Spi/Usb/usb/Inc/main.h:
+
+C:/git/usb_Spi/Usb/usb/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h:
+
+C:/git/usb_Spi/Usb/usb/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_def.h:
+
+C:/git/usb_Spi/Usb/usb/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h:
+
+C:/git/usb_Spi/Usb/usb/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio.h:
+
+C:/git/usb_Spi/Usb/usb/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio_ex.h:
+
+C:/git/usb_Spi/Usb/usb/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma.h:
+
+C:/git/usb_Spi/Usb/usb/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma_ex.h:
+
+C:/git/usb_Spi/Usb/usb/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_cortex.h:
+
+C:/git/usb_Spi/Usb/usb/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h:
+
+C:/git/usb_Spi/Usb/usb/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h:
+
+C:/git/usb_Spi/Usb/usb/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h:
+
+C:/git/usb_Spi/Usb/usb/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pcd.h:
+
+C:/git/usb_Spi/Usb/usb/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_ll_usb.h:
+
+C:/git/usb_Spi/Usb/usb/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pcd_ex.h:
+
+C:/git/usb_Spi/Usb/usb/Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_def.h:
+
+C:/git/usb_Spi/Usb/usb/Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_core.h:
+
+C:/git/usb_Spi/Usb/usb/Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_ioreq.h:
+
+C:/git/usb_Spi/Usb/usb/Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_ctlreq.h:
